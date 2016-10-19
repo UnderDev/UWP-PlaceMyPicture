3579de6db43eacb59e7c7b63fc7be2b2b1f1e5b6
UnderDev <scottcoyne@hotmail.com>
2016-10-19 18:05:49 +0100

    Facebook api auth + retive user pictures

---
+++ PlaceMyPicture/DeserializeJson.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static testApi_Facebook.PicturePlaceModel;
+
+namespace PlaceMyPicture
+{
+    class DeserializeJson
+    {
+        //Gets the class PicturePlaceObject and parses through the json responce Deserializing it into the class
+        public static PicturePlaceObject FromJson(string jsonText)
+        {
+            dynamic jsonObject = JsonConvert.DeserializeObject<PicturePlaceObject>(jsonText);
+            return jsonObject;
+        }
+    }
+}
--- PlaceMyPicture/MainPage.xaml.cs
+++ PlaceMyPicture/MainPage.xaml.cs
@@ -5,13 +5,19 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Security.Authentication.Web;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
+using winsdkfb;
+using winsdkfb.Graph;
+using static testApi_Facebook.PicturePlaceModel;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +31,79 @@ namespace PlaceMyPicture
         public MainPage()
         {
             this.InitializeComponent();
+            OnLogin();
+        }
+        private async void OnLogin()
+        {
+            string Sid = WebAuthenticationBroker.GetCurrentApplicationCallbackUri().ToString();
+            FBSession session = FBSession.ActiveSession;
+
+            session.WinAppId = Sid;//not used (only for windows 8 phone etc)
+            session.FBAppId = (1083564175045990).ToString();//AppId From App Created on facebook
+
+            List<String> permissionList = new List<String>();//list of all the permissions needed from the user
+            permissionList.Add("public_profile");
+            permissionList.Add("user_likes");
+            permissionList.Add("user_location");
+            permissionList.Add("user_photos");
+            permissionList.Add("publish_actions");
+            permissionList.Add("email");
+
+            FBPermissions permissions = new FBPermissions(permissionList);
+
+            var result = await session.LoginAsync(permissions);
+            if (result.Succeeded)
+            {
+                string name = session.User.Name;
+                OnGet();//once the user has given permissin and logged on
+            }
+        }
+
+        private async void OnGet()
+        {
+            string endpoint = "/me";//where the url starts from 
+
+
+
+            //parameters.offset = 10;
+
+
+            PropertySet parameters = new PropertySet();
+            //me? fields = photos{ source,place}
+            parameters.Add("fields", "photos{source,place}");//the fields you want to get
+            //parameters.Add("fields", "photos{images}");//the fields you want to get
+
+
+            FBSingleValue value = new FBSingleValue(endpoint, parameters, DeserializeJson.FromJson);//send the request and get back a JSON responce
+            FBResult graphResult = await value.GetAsync();//check to see if the Requets Succeeded 
+
+            if (graphResult.Succeeded)
+            {
+                PicturePlaceObject profile = graphResult.Object as PicturePlaceObject;
+                var lastImg = profile.photos.data.Count;
+
+                //data[24] is the 24th image
+                //string source = profile.photos.data[1].source.ToString();
+
+                pic1.Source = new BitmapImage(new Uri(profile.photos.data[1].source.ToString(), UriKind.Absolute));
+                pic2.Source = new BitmapImage(new Uri(profile.photos.data[2].source.ToString(), UriKind.Absolute));
+                pic3.Source = new BitmapImage(new Uri(profile.photos.data[3].source.ToString(), UriKind.Absolute));
+                pic4.Source = new BitmapImage(new Uri(profile.photos.data[4].source.ToString(), UriKind.Absolute));
+
+                pic5.Source = new BitmapImage(new Uri(profile.photos.data[5].source.ToString(), UriKind.Absolute));
+                pic6.Source = new BitmapImage(new Uri(profile.photos.data[6].source.ToString(), UriKind.Absolute));
+                pic7.Source = new BitmapImage(new Uri(profile.photos.data[7].source.ToString(), UriKind.Absolute));
+                pic8.Source = new BitmapImage(new Uri(profile.photos.data[8].source.ToString(), UriKind.Absolute));
+
+
+                //MessageDialog dialog = new MessageDialog(source);
+                // await dialog.ShowAsync();
+            }
+            else
+            {
+                MessageDialog dialog = new MessageDialog("Couldnt Find!");
+                await dialog.ShowAsync();
+            }
         }
     }
 }
