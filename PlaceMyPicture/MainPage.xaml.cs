7c7649086eee93be714cf683031b7c2e8f43b31b
UnderDev <scottcoyne@hotmail.com>
2016-10-20 20:29:24 +0100

    Added Bing Map and Custom Image pins by using the Long/Lat from FaceBook images

--- PlaceMyPicture/MainPage.xaml.cs
+++ PlaceMyPicture/MainPage.xaml.cs
@@ -19,6 +19,14 @@ using winsdkfb;
 using winsdkfb.Graph;
 using static testApi_Facebook.PicturePlaceModel;
 
+//maps 
+using Windows.UI.Xaml.Controls.Maps;
+using Windows.Devices.Geolocation;
+using Windows.Services.Maps;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml.Shapes;
+using Windows.UI;
+
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace PlaceMyPicture
@@ -28,11 +36,17 @@ namespace PlaceMyPicture
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        int picNum= 0;
+        PicturePlaceObject profile;
         public MainPage()
         {
             this.InitializeComponent();
             OnLogin();
         }
+
+        /*
+         * Facebook LogIn Authorization/Permissions
+         */
         private async void OnLogin()
         {
             string Sid = WebAuthenticationBroker.GetCurrentApplicationCallbackUri().ToString();
@@ -57,20 +71,20 @@ namespace PlaceMyPicture
                 string name = session.User.Name;
                 OnGet();//once the user has given permissin and logged on
             }
+            else
+            {
+                //Tell user they must log in to use the app
+            }
         }
 
         private async void OnGet()
         {
             string endpoint = "/me";//where the url starts from 
 
-
-
-            //parameters.offset = 10;
-
-
             PropertySet parameters = new PropertySet();
             //me? fields = photos{ source,place}
             parameters.Add("fields", "photos{source,place}");//the fields you want to get
+            parameters.Add("after", "TVRBeE5UWTVPRGcxTURJd01UQTFNVEU2TVRRMk5UVXhNRFV6T0Rvek9UUXdPRGsyTkRBMk5EYzRNelk9");
             //parameters.Add("fields", "photos{images}");//the fields you want to get
 
 
@@ -79,7 +93,7 @@ namespace PlaceMyPicture
 
             if (graphResult.Succeeded)
             {
-                PicturePlaceObject profile = graphResult.Object as PicturePlaceObject;
+                profile = graphResult.Object as PicturePlaceObject;
                 var lastImg = profile.photos.data.Count;
 
                 //data[24] is the 24th image
@@ -89,21 +103,83 @@ namespace PlaceMyPicture
                 pic2.Source = new BitmapImage(new Uri(profile.photos.data[2].source.ToString(), UriKind.Absolute));
                 pic3.Source = new BitmapImage(new Uri(profile.photos.data[3].source.ToString(), UriKind.Absolute));
                 pic4.Source = new BitmapImage(new Uri(profile.photos.data[4].source.ToString(), UriKind.Absolute));
+                foreach (var p in profile.photos.data)
+                {
+                    //Dont add pictures that dont have a long/lat
+                    if (p.place != null) {
+                        BitmapImage img = new BitmapImage(new Uri(p.source.ToString(), UriKind.Absolute));
+                        double lon = p.place.location.longitude;
+                        double lat = p.place.location.latitude;
+                        AddMapPin(img,lon,lat);
+                    }
+                }
 
-                pic5.Source = new BitmapImage(new Uri(profile.photos.data[5].source.ToString(), UriKind.Absolute));
-                pic6.Source = new BitmapImage(new Uri(profile.photos.data[6].source.ToString(), UriKind.Absolute));
-                pic7.Source = new BitmapImage(new Uri(profile.photos.data[7].source.ToString(), UriKind.Absolute));
-                pic8.Source = new BitmapImage(new Uri(profile.photos.data[8].source.ToString(), UriKind.Absolute));
-
-
-                //MessageDialog dialog = new MessageDialog(source);
-                // await dialog.ShowAsync();
             }
             else
             {
                 MessageDialog dialog = new MessageDialog("Couldnt Find!");
                 await dialog.ShowAsync();
             }
+        }
+
+        private void AddMapPin(BitmapImage img, double lon, double lat)
+        {
+            BasicGeoposition location = new BasicGeoposition();
+            location.Latitude = lat;
+            location.Longitude = lon;
+           
+            //Create the New Pin
+            CreateNewPin(location, img);
+
+            //Map.MapElements.Add(mapIcon);
+            Map.Center = new Geopoint(location);//center the map on the given location (MABY CURRENT)
+        }
+
+        public void CreateNewPin(BasicGeoposition location, BitmapImage urlImage)
+        {
+            ImageBrush imageBrush = new ImageBrush();
+            imageBrush.ImageSource = urlImage;
+
+            Button btn = new Button();
+            btn.Click += Btn_Click;
+            btn.Opacity = 0;
+            btn.Width = 24;
+            btn.Height = 24;
+            int t = 0;
+
+            var pinDesign = new Grid()
+            {
+                Height = 30,
+                Width = 30,
+                Margin = new Windows.UI.Xaml.Thickness(-10),
+                Name = t++.ToString(),
+            };
+
+            pinDesign.Children.Add(new Ellipse()
+            {
+                Fill = imageBrush,
+                Stroke = new SolidColorBrush(Colors.White),
+                StrokeThickness = 1,
+                Width = 24,
+                Height = 24,        
+            });
+            pinDesign.Children.Add(btn);
+
+            //pinDesign.Children.Add(new Image()
+            //{
+            //    Width = 30,                
+            //    Source = new BitmapImage(new Uri(urlImage, UriKind.Absolute)),
+            //    HorizontalAlignment = Windows.UI.Xaml.HorizontalAlignment.Center,
+            //    VerticalAlignment = Windows.UI.Xaml.VerticalAlignment.Center
+            //});
+
+            MapControl.SetLocation(pinDesign, new Geopoint(location));
+            Map.Children.Add(pinDesign);
+        }
+
+        private void Btn_Click(object sender, RoutedEventArgs e)
+        {           
+            pic4.Source = new BitmapImage(new Uri(profile.photos.data[picNum++].source.ToString(), UriKind.Absolute));
         }
     }
 }
